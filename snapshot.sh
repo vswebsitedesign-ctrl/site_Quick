bb1fa8831ec38ca5080cda879623d53a68b8d14e
vswebsitedesign-ctrl <vswebsitedesign@gmail.com>
2026-06-13 07:54:43 +0100

    Add .gitignore (snapshots/, engine_snapshots/, key.json), fix snapshot.sh slash-in-label bug

--- snapshot.sh
+++ snapshot.sh
@@ -2,6 +2,7 @@
 LABEL="${1:-snapshot}"
 TS=$(date +%Y%m%d_%H%M%S)
 DIR="snapshots/${TS}_${LABEL// /_}"
+ DIR="${DIR//\//_}"
 mkdir -p "$DIR"
 cp -r data theme _includes "$DIR/" 2>/dev/null
 echo "Snapshot saved to $DIR"
